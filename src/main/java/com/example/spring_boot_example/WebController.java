077813c5b5a3dc33a7a906f2cb76833da3b449ed
vaibhav <vaibhav.gautam2308@gmail.com>
2021-06-15 19:44:36 +0530

    Created Web Controller to be able to handle POST and GET requsets

---
+++ src/main/java/com/example/spring_boot_example/WebController.java
@@ -0,0 +1,30 @@
+package com.example.spring_boot_example;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class WebController {
+    @Autowired
+    private CustomerRepository customerRepository;
+
+    @PostMapping("/add")
+    public String addCustomer(@RequestParam String first, @RequestParam String last){
+        Customer customer = new Customer();
+        customer.setFirstName(first);
+        customer.setLastName(last);
+        customerRepository.save(customer);
+        return "Added new customer to repository";
+    }
+
+    @GetMapping("/list")
+    public Iterable<Customer> getCustomers(){
+        return customerRepository.findAll();
+    }
+
+    @GetMapping("/find/{id}")
+    public Customer findCustomerById(@PathVariable Integer id)
+    {
+        return customerRepository.findCustomersById(id);
+    }
+}
